655d8c538be8955585d21a1616bc64df41c50735
Thomas <thomas.standaert@kuleuven.vib.be>
2019-12-06 11:05:54 +0100

    Adding day1 of advent of code

---
+++ Day1_Part2.py
@@ -0,0 +1,16 @@
+import math
+
+def calculate_fuel(mass):
+    return math.floor(mass/3) - 2
+
+def masses_and_fuel(mass):
+    pass
+
+def sum_of_masses(input_file):
+    with open(input_file) as file:
+        sum_of_fuel=0
+        for line in file:
+            sum_of_fuel += calculate_fuel(int(line))
+    return sum_of_fuel
+
+print(sum_of_masses('Day1_InputFile'))
