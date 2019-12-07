8d4ece01805c38b7462b7707282324efa0ed7b2f
Thomas <thomas.standaert@kuleuven.vib.be>
2019-12-07 16:51:37 +0100

    Finished day2 part1 challenge

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -17,7 +17,7 @@ def multiplication(instructions, array):
 def calculator_case(input):
     characters = input.split(',')
     range = 0
-    while range < len(characters) - 3:
+    while range < len(characters):
         four_characters = characters[range:range+4]
         opcode = four_characters[0]
         if opcode == '1':
@@ -25,22 +25,17 @@ def calculator_case(input):
         elif opcode == '2':
             characters = multiplication(four_characters, characters)
         elif opcode == '99':
-            return characters
+            break
         range += 4
-
-
-
-    #return characters
+    return characters
 
 
 
 # Read File + main program
 def main_program(inputFile):
     with open(inputFile) as file:
-        pass
+        for line in file:
+            return calculator_case(line)
 
 
-print(calculator_case("1,9,10,3,2,3,11,0,99,30,40,50"))
-print(calculator_case("1,0,0,0,99"))
-print(calculator_case("2,4,4,5,99,0"))
-print(calculator_case("1,1,1,4,99,5,6,0,99"))
+print(main_program('Day2_InputFile'))
